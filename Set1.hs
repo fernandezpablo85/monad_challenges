21130a8b66e1c469071bfd243f9ddf21b148c53f
Pablo Fernandez <fernandezpablo85@gmail.com>
2016-02-23 01:07:52 -0300

    random number generation

--- Set1.hs
+++ Set1.hs
@@ -5,6 +5,8 @@ module Set1 where
 
   import MCPrelude
 
+  type Gen a = Seed -> (a, Seed)
+
   allRandsFor :: Seed -> [Integer]
   allRandsFor s = number : allRandsFor s2
     where (number, s2) = rand s
@@ -15,9 +17,8 @@ module Set1 where
   fiveRands :: [Integer]
   fiveRands = take 5 allRandsForSeedOne
 
-  randLetter :: Seed -> (Char, Seed)
-  randLetter s = (toLetter n, newSeed)
-    where (n, newSeed) = rand s
+  randLetter :: Gen Char
+  randLetter = generalA toLetter rand
 
   allStringsFor :: Seed -> String
   allStringsFor n = letter : allStringsFor seed
@@ -26,3 +27,37 @@ module Set1 where
   randString3 :: String
   randString3 = take 3 allStringsForSeedOne
     where allStringsForSeedOne = allStringsFor $ mkSeed 1
+
+  generalA :: (a -> b) -> Gen a -> Gen b
+  generalA f g s = (f x, s')
+    where (x, s') = g s
+
+  randEven :: Gen Integer
+  randEven = generalA (* 2) rand
+
+  randOdd :: Gen Integer
+  randOdd = generalA (+ 1) randEven
+
+  randTen :: Gen Integer
+  randTen = generalA (* 10) randOdd
+
+  randPair :: Gen (Char, Integer)
+  randPair s = ((c, i), s'')
+    where (c, s') = randLetter s
+          (i, s'') = rand s'
+
+  generalPair :: Gen a -> Gen b -> Gen (a, b)
+  generalPair a b s = ((ax, bx), s'')
+    where (ax, s') = a s
+          (bx, s'') = b s'
+
+  randPair' :: Gen (Char, Integer)
+  randPair' = generalPair2 randLetter rand
+
+  generalB :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+  generalB f a b s = (f ax bx, s'')
+    where (ax, s') = a s
+          (bx, s'') = b s'
+
+  generalPair2 :: Gen a -> Gen b -> Gen (a, b)
+  generalPair2 a b = generalB (\x y -> (x, y)) a b

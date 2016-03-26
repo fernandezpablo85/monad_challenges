577f835b8da997a5198260bb235280548c82ab18
Pablo Fernandez <fernandezpablo85@gmail.com>
2016-03-26 18:27:33 -0300

    fourth and fifth steps of ex2

--- Set2.hs
+++ Set2.hs
@@ -54,4 +54,41 @@ module Set2 where
 
   -- Fourth.
 
+  --chain :: (a -> Maybe b) -> Maybe a -> Maybe b
+  --chain _ Nothing = Nothing
+  --chain f (Just a) = f a
 
+  link :: Maybe a -> (a -> Maybe b) -> Maybe b
+  link Nothing _ = Nothing
+  link (Just a) f = f a
+
+  maxFromTail :: Ord a => [a] -> Maybe a
+  maxFromTail xs = link tail maximumMay
+    where tail = tailMay xs
+
+  queryGreek2 :: GreekData -> String -> Maybe Double
+  queryGreek2 gdata key = link pair (\(x,y) -> divMay y x)
+    where pair = link head (\h -> link maxTail (\m -> Just (fromIntegral(h), fromIntegral(m))))
+          maxTail = link tail maximumMay
+          tail = link list tailMay
+          head = link list headMay
+          list = lookupMay key gdata
+
+  -- Fifth.
+  type Name = String
+
+  ylink :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+  ylink f a b = link a (\a' -> link b (\b' -> mkMaybe (f a' b')))
+
+  mkMaybe :: a -> Maybe a
+  mkMaybe a = Just a
+
+  tupled :: Maybe a -> Maybe b -> Maybe (a, b)
+  tupled Nothing _ = Nothing
+  tupled _ Nothing = Nothing
+  tupled (Just a) (Just b) = mkMaybe (a, b)
+
+  addSalaries :: [(Name, Integer)] -> Name -> Name -> Maybe Integer
+  addSalaries ss name1 name2 = ylink (+) salary1 salary2
+    where salary1 = lookupMay name1 ss
+          salary2 = lookupMay name2 ss
